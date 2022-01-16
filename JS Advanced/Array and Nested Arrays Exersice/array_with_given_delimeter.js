27688b133972b42e3c91132cc90bb0c66998d10a
Bistrev Lyubomir <lubo_bistrev@abv.bg>
2022-01-16 20:26:08 +0100

    Delete array_with_given_delimeter.js

--- JS Advanced/Array and Nested Arrays Exersice/array_with_given_delimeter.js
+++
@@ -1,6 +0,0 @@
-function delimeter(sequence, symbol) {
-    console.log(sequence.join(symbol))
-}
-
-delimeter(['How about no?', 'I','will', 'not', 'do', 'it!'], '_')
-delimeter(['One', 'Two', 'Three', 'Four', 'Five'], '-')
