5e7176270e3d66b2c742634ec53b1bc1286bd659
Bistrev Lyubomir <lubo_bistrev@abv.bg>
2022-01-16 20:30:05 +0100

    Create construction_crew.js

---
+++ JS Advanced/03 Objects Exercise/construction_crew.js
@@ -0,0 +1,29 @@
+function worker(obj) {
+    if (obj.dizziness) {
+        obj.levelOfHydrated += obj.weight * obj.experience * 0.1;
+        obj.dizziness = false
+    }
+    return obj
+
+}
+
+console.log(worker({ weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true }
+  ))
+console.log("-------------------")
+
+  console.log(worker({ weight: 120,
+    experience: 20,
+    levelOfHydrated: 200,
+    dizziness: true }
+  ))
+
+  console.log("-------------------")
+
+console.log(  worker({ weight: 95,
+    experience: 3,
+    levelOfHydrated: 0,
+    dizziness: false }
+  ))
