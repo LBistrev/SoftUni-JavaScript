4c93e24c3cdea25110f7eee6c7be8a91193c5790
Bistrev Lyubomir <lubo_bistrev@abv.bg>
2022-01-16 21:09:42 +0100

    remov

--- JS Advanced/07 UnitTestExercise/05.Notification/app.js
+++
@@ -1,9 +0,0 @@
-function notify(message) {
-  const resultDiv = document.getElementById("notification");
-  resultDiv.textContent = message;
-  resultDiv.style.display = "block";
-
-  resultDiv.addEventListener("click", () => {
-    resultDiv.style.display = "none"
-  })
-}
